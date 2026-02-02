e20dc81e511e4abd63c0f188ffbc61f38fe9aec3
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2026-02-02 14:05:45 +0100

    fix bug in function parameter

--- code/preprocess/run_boosted_preproc_all_dates.sh
+++ code/preprocess/run_boosted_preproc_all_dates.sh
@@ -5,7 +5,7 @@ scenario="historical"
 conda activate eng_boost
 for date in "1996-01-20" "1996-01-23" "1996-01-26" "1996-01-29" "1996-02-01"  
     do
-    python preprocess_eng_data.py ${date} ${realization} SSP370
+    python preprocess_eng_data.py ${date} ${realization} ${scenario}
     python preprocess_CESM_bced_boosted.py ${date} ${realization} ${scenario}
 done
 conda deactivate
