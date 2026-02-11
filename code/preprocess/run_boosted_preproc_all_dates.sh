687fa7e97c240bb08c3caff77b5fd887a3ad7975
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2026-02-11 18:11:44 +0100

    changed boosted cases

--- code/preprocess/run_boosted_preproc_all_dates.sh
+++ code/preprocess/run_boosted_preproc_all_dates.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-realization="B"
-scenario="historical"
+realization="A"
+scenario="SSP370"
 conda activate eng_boost
-for date in "1996-01-20" "1996-01-23" "1996-01-26" "1996-01-29" "1996-02-01"  
+for date in  "2088-12-02" "2088-12-05" "2088-12-08" "2088-12-11" "2081-12-26" "2081-12-29" "2082-01-01" "2082-01-04" #"1996-01-20" "1996-01-23" "1996-01-26" "1996-01-29" "1996-02-01"
     do
     python preprocess_eng_data.py ${date} ${realization} ${scenario}
     python preprocess_CESM_bced_boosted.py ${date} ${realization} ${scenario}
